f365f447c2c9342400da03db57e012811a229900
Keiji, Yoshimi <walf443@gmail.com>
2009-03-28 19:44:15 +0900

    initial import.

---
+++ lib/function_importer.rb
@@ -0,0 +1,86 @@
+# add module a method to export function.
+module FunctionExporter
+  # Example:
+  #   module Utils
+  #     extend FunctionExporter
+  #     
+  #     def escape str
+  #       "escaped_#{str}"
+  #     end
+  #   end
+  #
+  #   module Foo
+  #     Utils.export self, :escape
+  #
+  #     def run
+  #       p(escape('str')) #=> "escaped_str"
+  #     end
+  #   end
+  #
+  #   # you can rename method when argument is Hash.
+  #   module Bar
+  #     Utils.export self, :escape => :my_escape
+  #
+  #     def run
+  #       p(my_escape('str')) #=> "escaped_str"
+  #     end
+  #   end
+  #
+  def export context, *args
+    parent_mod = create_export_module(*args)
+    context.module_eval do
+      include parent_mod
+    end
+  end
+
+  def create_export_module *args
+    parent_mod = self.dup
+
+    method_of = {};
+    case args.first
+    when Hash
+      args.first.each do |key, val|
+        method_of[key.to_s] = val
+      end
+    else
+      args.each do |i| 
+        method_of[i.to_s] = true
+      end
+    end
+
+    no_need_methods = []
+    parent_mod.instance_methods.each do |meth|
+      unless method_of[meth.to_s]
+        no_need_methods.push meth
+      end
+    end
+
+    parent_mod.module_eval do
+      # renate method.
+      method_of.each do |key, val|
+        next if val.kind_of? TrueClass
+        alias_method val, key
+        undef_method key
+      end
+      no_need_methods.each do |meth|
+        undef_method meth
+      end
+    end
+
+    parent_mod
+  end
+end
+
+# syntax suger for import function.
+module FunctionImporter
+
+  def import_function mod, *funcs
+    include(mod.create_export_module(*funcs))
+  end
+
+  def import_module_function mod, *funcs
+    extend(mod.create_export_module(*funcs))
+  end
+
+  private :import_function, :import_module_function
+end
---
+++ spec/function_exporter_spec.rb
@@ -0,0 +1,43 @@
+require File.join(File.dirname(__FILE__), 'spec_helper')
+
+describe FunctionExporter do
+  before :all do
+    @module = Module.new
+    @module.module_eval do
+      extend FunctionExporter
+
+      define_method :escape do |str|
+        "escaped_#{str}"
+      end
+
+    end
+  end
+
+  describe 'export a function' do
+    before do
+      @export_mod = @module.create_export_module :escape
+    end
+
+    it 'should return Module' do
+      @export_mod.should be_kind_of(Module)
+    end
+
+    it 'should have #escape as public' do
+      @export_mod.public_instance_methods.any? {|i| i.to_s == 'escape' }.should be_true
+    end
+  end
+
+  describe 'export a function with rename' do
+    before do
+      @export_mod = @module.create_export_module :escape => :my_escape
+    end
+
+    it 'should not have #escape' do
+      @export_mod.instance_methods.all? {|i| i.to_s != 'escape' }.should be_true
+    end
+
+    it 'should have #my_escape as public' do
+      @export_mod.instance_methods.any? {|i| i.to_s == 'my_escape' }.should be_true
+    end
+  end
+end
